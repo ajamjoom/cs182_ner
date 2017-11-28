986b5d3920da491c4314d1b4a42d050e3d73ad34
Abdulrahman Jamjoom <ajamjoom@college.harvard.edu>
2017-11-27 23:04:37 -0500

    new init

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def verify():
             return "Verification token mismatch", 403
         return request.args["hub.challenge"], 200
 
-    return "Hello world", 200
+    return "CS182 Final Project | NER with HMM and fb messenger user output | by Abdulrahman Jamjoom and Josh Kupersmith", 200
 
 
 @app.route('/', methods=['POST'])
