369ff5c16ee32fc60988a387a0456edd39d4449b
Abdulrahman Jamjoom <ajamjoom@college.harvard.edu>
2017-11-27 21:46:11 -0500

    testing heroku connection

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def verify():
             return "Verification token mismatch", 403
         return request.args["hub.challenge"], 200
 
-    return "Hello testing", 200
+    return "Hello biatch", 200
 
 
 @app.route('/', methods=['POST'])
