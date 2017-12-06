4f7b496a34a39a9f273d2c7eba67e0f25126de7a
Abdulrahman Jamjoom <ajamjoom@college.harvard.edu>
2017-12-06 06:28:37 -0500

    old ner model should work in messenger.. testing

--- app.py
+++ app.py
@@ -15,87 +15,6 @@ from collections import Counter
 
 app = Flask(__name__)
 
-# pos_probs = {}
-# shape_probs = {}
-# word_probs = {}
-
-# def dict_from_csv(csv_name):
-#     with open(csv_name, 'rb') as csv_file:
-#         init_dict = dict(csv.reader(csv_file))
-#         new_dict = {}
-#         for key, value in init_dict.iteritems():
-#             new_dict[unicode(key, 'utf-8')] = ast.literal_eval(value)
-#     return new_dict
-
-# def camel(s):
-#     return (s != s.lower() and s != s.upper())
-
-# def local_shapes(spacy_shape):
-#     if spacy_shape.islower():
-#         return u'lowercase'
-#     elif spacy_shape.isupper():
-#         return u'uppercase'
-#     elif not spacy_shape.isnumeric():
-#        return u'mixedcase' 
-#     elif spacy_shape[0].isupper():
-#         return u'capitalized'
-#     elif spacy_shape.isnumeric():
-#         return u'number'
-#     elif camel(spacy_shape):
-#         return u'camelcase'
-#     elif spacy_shape[len(spacy_shape)-1] == '.':
-#         return u'ending-dot',
-#     elif '-' in spacy_shape:
-#         return u'contains-hyphen'
-#     elif spacy_shape in string.punctuation:
-#         return u'punct'
-#     else:
-#         return u'other'
-
-# def messenger_ner(sentence):
-#     # Populate the pos, word, and shape dicts from their CSV files
-#     pos_probs = dict_from_csv('pos.csv')
-#     shape_probs = dict_from_csv('shape.csv')
-#     word_probs = dict_from_csv('word.csv')
-
-#     nlp = spacy.load('en')
-#     doc = nlp(sentence.decode('utf-8'))
-
-#     sentence_dict = {}
-#     for token in doc:
-#         local_shapes(token.shape_)
-#         sentence_dict[token] = (token.tag_, local_shapes(token.shape_))
-
-#     # print sentence_dict
-#     tagged_sentence = {}
-#     for word, value in sentence_dict.iteritems():
-#         word_pos, word_shape = value
-#         prob = 0.0
-#         max_prob = 0.0
-#         max_tag = ''
-#         for tag in tag_list:  
-#             # try, except to ignore one value when a word has not been seen before!
-#             try:      
-#                 pos_p = pos_probs[word_pos][tag]
-#             except:
-#                 pos_p = 1.0
-#             try:
-#                 shape_p = shape_probs[word_shape][tag]
-#             except:
-#                 shape_p = 1.0
-#             try:
-#                 word_p = word_probs[word][tag]
-#             except:
-#                 word_p = 1.0
-#             prob = pos_p * shape_p * word_p
-
-#             if prob > max_prob:
-#                 max_prob = prob
-#                 max_tag = tag
-#         tagged_sentence[word] = max_tag
-
-#     return tagged_sentence
-
 @app.route('/', methods=['GET'])
 def verify():
     # when the endpoint is registered as a webhook, it must echo back
@@ -104,52 +23,6 @@ def verify():
         if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
             return "Verification token mismatch", 403
         return request.args["hub.challenge"], 200
-
-    # Doesn't account for type = abbreviation
-    # def shape(word):
-    #     if re.match('[0-9]+(\.[0-9]*)?|[0-9]*\.[0-9]+$', word, re.UNICODE):
-    #         return u'number'
-    #     elif '.' == word:
-    #         return u'ending-dot'
-    #     elif re.match('\W+$', word, re.UNICODE):
-    #         return u'punct'
-    #     elif re.match('\w+$', word, re.UNICODE):
-    #         if '-' in word:
-    #             return u'contains-hyphen'
-    #         elif word.isupper():
-    #             return u'uppercase'
-    #         elif word.islower():
-    #             return u'lowercase'
-    #         elif word[0].isupper():
-    #             return u'capitalized'
-    #         elif word[0].islower():
-    #             return u'camelcase'
-    #         else:
-    #             return u'mixedcase'
-    #     else:
-    #         return u'other'
-
-    # tokens = nltk.word_tokenize("Dive into NLTK: Part-of-speech tagging POS Tagger")
-                            
-    # pos_tags = nltk.pos_tag(tokens)
-    
-    # shapes = []
-    # for token in tokens:
-    #     token_shape = shape(token)
-    #     shapes.append((token, token_shape))
-
-    # log("TOKENS")
-    # log(tokens)
-    # log("POS_TAGS")
-    # log(pos_tags)
-    # log("TOKEN_SHAPES")
-    # log(shapes)
-    # print ("TOKENS")
-    # print (tokens)
-    # print ("POS_TAGS")
-    # print (pos_tags)
-    # print ("TOKEN_SHAPES")
-    # print (shapes)
     return "CS182 Final Project | NER with HMM and fb messenger user output | by Abdulrahman Jamjoom and Josh Kupersmith", 200
 
 
@@ -157,7 +30,6 @@ def verify():
 def webhook():
 
     # endpoint for processing incoming messaging events
-
     data = request.get_json()
     # log(data)
 
@@ -194,6 +66,23 @@ def webhook():
                             # reply to user with error if the text is not in the correct format
                             send_message(sender_id, "Thank you for improving our algorithm!")     
                         else:
+                            pos_probs = {}
+                            shape_probs = {}
+                            word_probs = {}
+
+                            def dict_from_csv(csv_name):
+                                with open(csv_name, 'rb') as csv_file:
+                                    init_dict = dict(csv.reader(csv_file))
+                                    new_dict = {}
+                                    for key, value in init_dict.iteritems():
+                                        new_dict[unicode(key, 'utf-8')] = ast.literal_eval(value)
+                                return new_dict
+
+                            # Populate the pos, word, and shape dicts from their CSV files
+                            pos_probs = dict_from_csv('pos.csv')
+                            shape_probs = dict_from_csv('shape.csv')
+                            word_probs = dict_from_csv('word.csv')
+
                             # Doesn't account for type = abbreviation
                             def shape(word):
                                 if re.match('[0-9]+(\.[0-9]*)?|[0-9]*\.[0-9]+$', word, re.UNICODE):
@@ -217,24 +106,68 @@ def webhook():
                                         return u'mixedcase'
                                 else:
                                     return u'other'
-            
-                            tokens = nltk.word_tokenize("Dive into NLTK: Part-of-speech tagging POS Tagger")
+
+                            def messenger_ner(sentence_lst):
+   
+                                # print sentence_dict
+                                tagged_sentence = ''
+                                for token_data in sentence_dict:
+                                    
+                                    word, word_pos, word_shape = token_data[0], token_data[1], token_data[2]
+                                    prob = 0.0
+                                    max_prob = 0.0
+                                    max_tag = ''
+                                    for tag in tag_list:  
+                                        # try, except to ignore one value when a word has not been seen before!
+                                        try:      
+                                            pos_p = pos_probs[word_pos][tag]
+                                        except:
+                                            pos_p = 1.0
+                                        try:
+                                            shape_p = shape_probs[word_shape][tag]
+                                        except:
+                                            shape_p = 1.0
+                                        try:
+                                            word_p = word_probs[word][tag]
+                                        except:
+                                            word_p = 1.0
+                                        prob = pos_p * shape_p * word_p
+
+                                        if prob > max_prob:
+                                            max_prob = prob
+                                            max_tag = tag
+                                    tagged_sentence += '('+word+','+max_tag+')'
+
+                                return tagged_sentence
+
+                            words = nltk.word_tokenize("Dive into NLTK: Part-of-speech tagging POS Tagger")
                             
-                            pos_tags = nltk.pos_tag(tokens)
+                            pos_tags_tuple = nltk.pos_tag(words)
+
+                            pos_tags = []
+                            for elmt in pos_tags_tuple:
+                                word, tag = elmt
+                                pos_tags.append(tag)
                             
                             shapes = []
                             for token in tokens:
                                 token_shape = shape(token)
-                                shapes.append((token, token_shape))
+                                shapes.append(token_shape)
 
+                            sentence_data = []
+                            for i in xrange(len(words)):
+                                sentence_dict.append([words[i], pos[i], shapes[i]])
+                                
                             log("TOKENS")
                             log(tokens)
                             log("POS_TAGS")
                             log(pos_tags)
                             log("TOKEN_SHAPES")
                             log(shapes)
-
-                            send_quickrep_message(sender_id, "tokens[0]")
+                            log("messenger_ner(sentence_data)")
+                            log(messenger_ner(sentence_data))
+                            
+                            send_quickrep_message(sender_id, messenger_ner(sentence_data))
 
                 if messaging_event.get("postback"):  # user clicked/tapped "postback" button in earlier message
                     pass
